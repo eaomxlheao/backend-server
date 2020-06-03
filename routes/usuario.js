bc25e73c9ea291974a11ce0aba8a24e68cf77370
EduArdO <eaomxlheao@gmail.com>
2020-06-02 20:16:24 -0500

    Put, actualizar, update

--- routes/usuario.js
+++ routes/usuario.js
@@ -27,6 +27,54 @@ app.get("/", (request, response, next) => {
 });
 
 ///===============================================
+/// Actualizar usuario
+///===============================================
+app.put("/:id", (request, response) => {
+    var id = request.params.id;
+    var body = request.body;
+
+    Usuario.findById(id, (err, usuario) => {
+        if (err) {
+            return response.status(500).json({
+                ok: true,
+                message: "Error al buscar usuario!",
+                errors: err,
+            });
+        }
+
+        if (!usuario) {
+            return response.status(401).json({
+                ok: true,
+                message: "El usuario con el id: " + id + " no existe!",
+                errors: { message: "No existe usuario con ese ID" },
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save((err, usuarioGuardado) => {
+            if (err) {
+                return response.status(400).json({
+                    ok: true,
+                    message: "Error al actualizar usuario!",
+                    errors: err,
+                });
+            }
+
+            usuarioGuardado.password = ":)";
+
+            //201=>Created
+            return response.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado,
+            });
+        });
+    });
+});
+
+///===============================================
 /// Crear usuario
 ///===============================================
 app.post("/", (request, response) => {
