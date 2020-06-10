382ec8b5beee37a799a8cd2d3523fdc1f0629768
EduArdO <eaomxlheao@gmail.com>
2020-06-10 14:23:49 -0500

    Validacion de credenciales Google

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = "cwHr:TU4=W;2W}bxlG9G";
+
+//GOOGLE
+module.exports.CLIENT_ID =
+    "731389811270-1o1en3guv7q2n80cjrrsi2kuj0a8fs1o.apps.googleusercontent.com";
--- routes/login.js
+++ routes/login.js
@@ -7,10 +7,53 @@ var app = express();
 
 var Usuario = require("../models/usuario");
 
+//Google
+var CLIENT_ID = require("../config/config").CLIENT_ID;
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(CLIENT_ID);
+
 //Routes
+///===============================================
+/// Autenticacion google
+///===============================================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload["sub"];
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        imagen: payload.picture,
+        google: true,
+    };
+}
+
+app.post("/google", async(request, response) => {
+    var token = request.body.token;
+    var googleUser = await verify(token).catch((e) => {
+        return response.status(403).json({
+            ok: false,
+            message: "token no valido",
+        });
+    });
+
+    return response.status(200).json({
+        ok: true,
+        message: "Peticion realizada correctamente",
+        googleUser: googleUser,
+    });
+});
 
 ///===============================================
-/// Obtener todos los usuarios
+/// Autenticacion normal
 ///===============================================
 app.post("/", (request, response) => {
     var body = request.body;
